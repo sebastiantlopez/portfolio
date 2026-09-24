43794e8d5471db0c10eafec62951ee41f58bc80e
Sebastian Lopez <332685213+sebastiantlopez@users.noreply.github.com>
2026-09-24 12:29:07 -0700

    Resize blob cursor and replace native pointer

--- app/blob-cursor.js
+++ app/blob-cursor.js
@@ -14,6 +14,6 @@ export default function BlobCursor(){
   window.addEventListener('pointermove',move,{passive:true});
   return()=>{window.removeEventListener('pointermove',move);blobs.current.forEach(el=>el&&gsap.killTweensOf(el))};
  },[]);
- const sizes=[34,74,48],inner=[10,18,13],opacity=[.5,.22,.34];
+ const sizes=[23,49,32],inner=[7,12,9],opacity=[.5,.22,.34];
  return <div className="blob-container" aria-hidden="true"><svg width="0" height="0"><filter id={id}><feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="18"/><feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -10"/></filter></svg><div className="blob-main" style={{filter:`url(#${id})`}}>{sizes.map((size,i)=><span className="blob" key={size} ref={el=>blobs.current[i]=el} style={{width:size,height:size,marginLeft:-size/2,marginTop:-size/2,opacity:opacity[i]}}><i style={{width:inner[i],height:inner[i],inset:(size-inner[i])/2}}/></span>)}</div></div>
 }
